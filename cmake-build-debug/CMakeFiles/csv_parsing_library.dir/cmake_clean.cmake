cf657a2145ee7f2f95a9238463ef69086731708d
mrduoduo <nmbh1122@126.com>
2020-11-10 17:58:58 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/csv_parsing_library.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/csv_parsing_library.dir/main.cpp.o"
+  "csv_parsing_library"
+  "csv_parsing_library.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/csv_parsing_library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <zconf.h>
+#include <cstring>
+
+
+using namespace std;
+
+
+void parsing(char *file_src) {
+    ifstream csv_file(file_src, std::fstream::binary);
+
+    int line_n = 0;
+    char line[256];
+
+    while (true) {
+        csv_file.getline(line, 256);
+        string line_string = line;
+        if (line_string.size() == 0) {
+            printf("The end\n");
+            break;
+        }
+
+        int sub = 0;
+        size_t i = 0;
+        std::size_t char_position = 0;
+        string sub_string;
+        while (true) {
+            int flag = 0;
+            char_position = line_string.find(",", i);
+
+            if (char_position == -1) {
+                char_position = line_string.find("\n", i);
+                flag = 1;
+            }
+
+            int size = char_position - i;
+            sub_string = line_string.substr(i, size);
+
+            printf("%d.%d:%s\n", line_n, sub, sub_string.c_str());
+
+            i = i + size + 1;
+            sub++;
+
+            if (flag) {
+                break;
+            }
+        }
+        line_n++;
+    }
+}
+
+int main() {
+    char *csv_src = "/home/zyx/workspace/csv_parsing_library/test.csv";
+    parsing(csv_src);
+    return 0;
+}
+
